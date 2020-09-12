813ebf1aaa2d3d93de1491d832d5bcb6d8109464
Radovan Bast <bast@users.noreply.github.com>
2020-09-12 20:22:46 +0200

    add jupyter_execute/ to exclude_patterns; fixes #40

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ extensions = [
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', 'jupyter_execute']
 
 
 # -- Options for HTML output -------------------------------------------------
