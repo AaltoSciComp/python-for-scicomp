29101bb54631e0639c41fafd57c0f051a81a1997
Richard Darst <rkd@zgib.net>
2021-10-11 18:39:07 +0300

    Add ASC branding

--- content/conf.py
+++ content/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx_rtd_theme_ext_color_contrast',
     'sphinx.ext.todo',
     'sphinx.ext.intersphinx',
+    'sphinx_aaltoscicomp_branding',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
